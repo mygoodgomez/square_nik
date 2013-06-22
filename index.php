06da4022b682a6bed92f0673aaa78d21fa0174ea
Nik Gomez <niksterg11@gmail.com>
2013-06-22 12:10:54 -0400

    improving styling of footer

--- index.php
+++ index.php
@@ -50,7 +50,10 @@
 		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 		'format' => '?paged=%#%',
 		'current' => max( 1, get_query_var('paged') ),
-		'total' => $wp_query->max_num_pages
+		'total' => $wp_query->max_num_pages,
+		'prev_text' => '« Prev',
+		'next_text' => 'Next »'
+
 	) );
 	?>
 </div>
